7ac12f12b7cb38b9e3b97d8b1bf2618a087ad69b
Dell <Dell@DESKTOP-4SB8VJN>
2024-03-18 19:56:42 +0530

    maven project

---
+++ src/main/java/com/obsquara/testing/ExcelCode.java
@@ -0,0 +1,33 @@
+package com.obsquara.testing;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class ExcelCode {
+	public static FileInputStream f;
+	public static XSSFWorkbook wb;
+	public static XSSFSheet sh;
+	
+	public static String readStringData(int i,int j) throws IOException{
+		f=new FileInputStream("C:\\Users\\Dell\\Desktop\\sample java programs\\mavenproject.xlsx");
+		wb = new XSSFWorkbook(f);
+		sh = wb.getSheet("Sheet1");
+		XSSFRow r=sh.getRow(i);
+		XSSFCell c=r.getCell(j);
+		return c.getStringCellValue();
+		
+	}
+public static double readNumericData(int i,int j) throws IOException{
+	f= new FileInputStream("C:\\Users\\Dell\\Desktop\\sample java programs\\mavenproject.xlsx");
+	wb =new XSSFWorkbook(f);
+	sh =wb.getSheet("sheet1");
+	XSSFRow r=sh.getRow(i);
+	XSSFCell c=r.getCell(j);
+	return c.getNumericCellValue();
+}
+}
